1cfb1567f083cacdeccd946b172c4eb373fa22da
hieu-nguyen-dss <hieu.nguyen@dssolution.jp>
2024-01-09 00:54:10 +0700

    update code

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -115,6 +115,19 @@ const App = () => {
       } catch (error) {
         // openSnackbar(SNACKBAR.ERROR, "Create cart failed");
       }
+    } else {
+      try {
+        await fetch(`http://localhost:8889/api/shopping-cart-items`, {
+          method: "DELETE",
+          body: formData,
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Cookie: `ms2a=${token}`,
+          },
+        });
+      } catch (error) {
+        // openSnackbar(SNACKBAR.ERROR, "Update cart failed");
+      }
     }
   };
   const handleUpdateCartQty = async (
